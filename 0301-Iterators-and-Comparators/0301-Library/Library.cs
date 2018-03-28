8aae73bc9b8b1e331ae63e86d580283e75223469
Milena Petkanova <milenaslaveikova78@gmail.com>
2018-03-28 20:11:11 +0300

    Add all lab exercises

---
+++ 0301-Iterators-and-Comparators/0301-Library/Library.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Library : IEnumerable<Book>
+{
+    private List<Book> books;
+
+    public Library(params Book[] books)
+    {
+        this.books = new List<Book>(books);
+    }
+
+    public IEnumerator<Book> GetEnumerator() => this.books.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+}
+
---
+++ 0301-Iterators-and-Comparators/0304-Book-Comparator/Library.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Library : IEnumerable<Book>
+{
+    private SortedSet<Book> books;
+    private BookComparator comparator;
+
+    public Library(params Book[] books)
+    {
+        this.books = new SortedSet<Book>(books, this.comparator);
+        this.comparator = new BookComparator();
+    }
+
+    public IEnumerator<Book> GetEnumerator() => new LibraryIterator(this.books);
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        private readonly List<Book> books;
+        private int currentIndex;
+
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.Reset();
+            this.books = new List<Book>();
+        }
+
+        public Book Current => this.books[currentIndex];
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose() { }
+
+        public bool MoveNext() => ++this.currentIndex < this.books.Count;
+
+        public void Reset() => this.currentIndex = -1;
+    }
+
+}
+
