2d6569f0734aca5b90083f85d29db75db1a488f8
Milena Petkanova <milenaslaveikova78@gmail.com>
2018-04-22 08:56:50 +0300

    Add author's solution, not mine

---
+++ RecyclingStation/Recycling Station_Авторско Решение/RecyclingStation/UI/ConsoleWriter.cs
@@ -0,0 +1,13 @@
+﻿namespace RecyclingStation.UI
+{
+    using RecyclingStation.Interfaces.UI;
+    using System;
+
+    public class ConsoleWriter : IWriter
+    {
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
